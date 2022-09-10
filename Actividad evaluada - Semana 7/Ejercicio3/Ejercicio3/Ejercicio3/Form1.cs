b05523783518efd097d9065fc7f1484ba7839f3a
Adiel <SMSS131422@ugb.edu.sv>
2022-09-10 16:31:49 -0600

    Actividad evaluada - Semana 7

---
+++ Actividad evaluada - Semana 7/Ejercicio3/Ejercicio3/Ejercicio3/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double número1 = Convert.ToDouble(textBox1.Text);
+            double número2 = Convert.ToDouble(textBox2.Text);
+            double suma = número1 + número2;
+            label3.Text = "La suma es: " + suma.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double número1 = Convert.ToDouble(textBox1.Text);
+            double número2 = Convert.ToDouble(textBox2.Text);
+            double resta = número1 - número2;
+            label3.Text = "La resta es: " + resta.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double número1 = Convert.ToDouble(textBox1.Text);
+            double número2 = Convert.ToDouble(textBox2.Text);
+            double multiplicacion = número1 * número2;
+            label3.Text = "La multiplicación es: " + multiplicacion.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double número1 = Convert.ToDouble(textBox1.Text);
+            double número2 = Convert.ToDouble(textBox2.Text);
+            double division = número1 / número2;
+            label3.Text = "La division es: " + division.ToString();
+        }
+    }
+}
---
+++ Actividad evaluada - Semana 7/Ejercicio4/Ejercicio4/Ejercicio4/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double número1 = Convert.ToDouble(textBox1.Text);
+            double número2 = Convert.ToDouble(textBox2.Text);
+
+            if (checkBox1.Checked == true) 
+            {
+                double suma = número1 + número2;
+                label3.Text = suma.ToString();
+            }
+
+            if (checkBox2.Checked == true) 
+            {
+                double resta = número1 - número2;
+                label3.Text = resta.ToString();
+            }
+
+            if (checkBox3.Checked == true)
+            {
+                double multiplicacion = número1 * número2;
+                label3.Text = multiplicacion.ToString();
+            }
+
+            if (checkBox4.Checked == true)
+            {
+                double division = número1 / número2;
+                label3.Text = division.ToString();
+            }
+        }
+    }
+}
